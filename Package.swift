3bed7b58ee548a758689096ad5da35228661ccfb
persona-identities <57889169+persona-identities@users.noreply.github.com>
2023-04-03 14:00:47 -0500

    Update Package.swift for 2.7.4

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     .binaryTarget(
       name: "PersonaNfc",
       url: "https://github.com/persona-id/inquiry-ios-nfc/releases/download/2.7.4/PersonaNfc.xcframework.zip",
-      checksum: "b03329ea5ac3610bd14131f2a409f9a53b571028bb93d4ecec60a4406dddf867"
+      checksum: "2f60a9e5b70738d29ea7a87733b72a22c47eb37a744747bacccbcc3ae523548e"
     )
   ]
 )
