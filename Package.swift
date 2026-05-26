588320041e8c6d9973f06cdeb32c0ee16940f694
mobile-build-persona <mobile-build@withpersona.com>
2026-05-26 07:58:02 -0600

    Bump to 2.48.1-beta.1

--- Package.swift
+++ Package.swift
@@ -13,8 +13,8 @@ let package = Package(
   targets: [
     .binaryTarget(
       name: "PersonaNfc",
-      url: "https://github.com/persona-id/inquiry-ios-nfc/releases/download/2.48.0/PersonaNfc.xcframework.zip",
-      checksum: "1b98c574897cdc37316ff6770c6ab450c31b86a283728ec3f590d10e457109bb"
+      url: "https://storage.googleapis.com/sdk.withpersona.com/ios/v2/releases/2.48.1-beta.1/PersonaNfc.xcframework.zip",
+      checksum: "c590cc88b6995ef4cd18b6ac88fd9f626640b80ae327c79e041f061a4d480019"
     )
   ]
 )
