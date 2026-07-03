bd559e66a04602e1eaf277518769f640c88b6a8a
mobile-build-persona <mobile-build@withpersona.com>
2026-07-03 10:39:22 -0600

    Update Package.swift for 2.50.0

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
   targets: [
     .binaryTarget(
       name: "PersonaNfc",
-      url: "https://github.com/persona-id/inquiry-ios-nfc/releases/download/2.50.0-RC/PersonaNfc.xcframework.zip",
+      url: "https://github.com/persona-id/inquiry-ios-nfc/releases/download/2.50.0/PersonaNfc.xcframework.zip",
       checksum: "a9aefb0f7c1505ef6ffa6c926ca0a8b60afea7c3129b7126f82e276b9f0beb42"
     )
   ]
